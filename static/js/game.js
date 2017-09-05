cb4b416b517af57f55cd5eaa0930cdd350e5fe4a
liangxu <liangxu@nle-tech.com>
2017-09-05 22:43:46 +0800

    complete chain

--- static/js/game.js
+++ static/js/game.js
@@ -10,24 +10,23 @@
     return context
   }
   // 小球链子的构造函数
-  var Chain = function (context) {
+  var Chain = function (ball, context) {
     this.context = context
     this.color = '#333333'
     this.initX = 0;
     this.initY = 0;
-    this.objX = 100;
-    this.objY = 100;
-    this.frequency = 4;
+    this.objX = 200;
+    this.objY = 200;
+    this.frequency = 6;
     this.initRadius = 10;
     this.radius = 5;
-    this._init()
+    this._init(ball);
   }
   // 链子的初始化方法
-  Chain.prototype._init = function () {
-    this._drawCircle(this.initX, this.initY, this.initRadius)
-    for (var i = 0, len = this.frequency; i < len; i++) {
-      this._drawCircle(this.initX + (this.initX - this.objX) / i, this.initY + (this.initY - this.objY) / i, this.radius)
-    }
+  Chain.prototype._init = function (ball) {
+    var ballCenter = ball._computedCenter()
+    this.initX = ballCenter.x - this.initRadius;
+    this.initY = ballCenter.y - this.initRadius;
   }
   // 绘制圆形方法
   Chain.prototype._drawCircle = function (x, y, radius) {
@@ -36,6 +35,16 @@
     this.context.arc(x, y, radius, 0, Math.PI * 2, true);
     this.context.closePath();
     this.context.fill();
+  }
+  // 绘制链子
+  Chain.prototype.draw = function (ball) {
+    var ballCenter = ball._computedCenter()
+    this.objX = ballCenter.x;
+    this.objY = ballCenter.y;
+    this._drawCircle(this.initX, this.initY, this.initRadius)
+    for (var i = 2, len = this.frequency + 2; i < len; i++) {
+      this._drawCircle(this.initX + (this.objX - this.initX) * i / len, this.initY + (this.objY - this.initY) * i / len, this.radius)
+    }
   }
   // 小球的构造函数
   var Ball = function (context) {
@@ -46,12 +55,18 @@
     this.initY = this.context.height / 2 - this.radius;
     this.x = 0;
     this.y = 0;
+    this.chain = null;
+    this.speed = 0;
+    this.runFlag = false;
+    this.offsetX = 0;
+    this.offsetY = 0;
     this._init();
   }
   // 小球初始化方法
   Ball.prototype._init = function () {
     this.x = this.initX;
     this.y = this.initY;
+    this.chain = new Chain(this, this.context)
     this.image.src = './static/image/ball_2.png';
     this._bindDrag();
   }
@@ -62,6 +77,10 @@
       y: this.y + this.radius,
     }
   }
+  // 计算距离
+  Ball.prototype._computedDistance = function (x, y, objX, objY) {
+    return Math.sqrt(Math.pow(objX - x, 2) + Math.pow(objY - y, 2))
+  }
   // 判断点击的是否是小球
   Ball.prototype._isClickSelf = function (e) {
     var offset = {
@@ -69,8 +88,7 @@
       y: e.clientY - this.y,
     }
     var center = this._computedCenter();
-
-    var is = Math.sqrt(Math.pow(center.x - e.clientX, 2) + Math.pow(center.y - e.clientY, 2)) < this.radius;
+    var is = this._computedDistance(e.clientX, e.clientY, center.x, center.y) < this.radius;
     return {
       is: is,
       offset: offset,
@@ -96,19 +114,38 @@
       }
     })
     document.body.addEventListener('mouseup', function (e) {
-      mouseDownFlag = false
+      mouseDownFlag = false;
+      var distance = that._computedDistance(e.clientX, e.clientY, that.initX, that.initY);
+      if (distance) {
+        that.offsetX = that.initX - e.clientX
+        that.offsetY = that.initY - e.clientY
+        that.speed = distance / 10;
+        that.switchRun(true);
+      }
     })
   }
   // 绘制小球
   Ball.prototype.draw = function () {
     this.context.clearRect(0, 0, this.context.width, this.context.height);
     this.context.drawImage(this.image, this.x, this.y, this.radius * 2, this.radius * 2);
+    this.chain && this.chain.draw(this)
+    this.runFlag && this._run()
+  }
+  // 小球运动
+  Ball.prototype._run = function () {
+    var offsetSum = (Math.abs(this.offsetX) + Math.abs(this.offsetY))
+    this.x += this.speed * this.offsetX / offsetSum;
+    this.y += this.speed * this.offsetY / offsetSum;
+    this.speed = this.speed - this.speed / 50
+  }
+  // 小球运动开关
+  Ball.prototype.switchRun = function (bool) {
+    this.runFlag = bool
   }
   // 主函数
   var main = function () {
     var context = initCanvas()
     var ball = new Ball(context)
-    // var chain = new Chain(context)
     // 开始动画
     animationId = window.requestAnimationFrame(function step() {
       ball.draw();
