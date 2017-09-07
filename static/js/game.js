3ea2fb1b3f6f5d85d602b23c5b19a899b48e81b4
liangxu <liangxu@nle-tech.com>
2017-09-07 22:35:31 +0800

    add BallObj

--- static/js/game.js
+++ static/js/game.js
@@ -39,7 +39,7 @@
   }
   // 绘制链子
   Chain.prototype.draw = function (ball) {
-    if(!this.life) return
+    if (!this.life) return
     var ballCenter = ball._computedCenter()
     this.objX = ballCenter.x;
     this.objY = ballCenter.y;
@@ -65,7 +65,9 @@
     this.speedX = 0;
     this.speedY = 0;
     this.runFlag = false;
-    this.impactObj = [];
+    this.impactObjArr = [];
+    this.objArr = [];
+    this.objNum = 10
     this._init();
   }
   // 小球初始化方法
@@ -74,10 +76,23 @@
     this.y = this.initY;
     this.image.src = './static/image/ball_2.png';
     this._bindDrag();
-    this._addImpactObj({x: 0}, true)
-    this._addImpactObj({x: this.context.width}, true)
-    this._addImpactObj({y: 0}, true)
-    this._addImpactObj({y: this.context.height}, true)
+    this.addImpactObj({
+      x: 0
+    }, true)
+    this.addImpactObj({
+      x: this.context.width
+    }, true)
+    this.addImpactObj({
+      y: 0
+    }, true)
+    this.addImpactObj({
+      y: this.context.height
+    }, true)
+    for(var i = 0, len = this.objNum; i < len; i++) {
+      var obj = new BallObj(this.context)
+      this.objArr.push(obj)
+      this.addImpactObj(obj, obj.type)
+    }
   }
   // 计算并返回小球中心坐标
   Ball.prototype._computedCenter = function () {
@@ -107,16 +122,18 @@
   }
   // 绑定小球拖拽的方法
   Ball.prototype._bindDrag = function () {
-    var mouseDownFlag = false
-    var _this = this
+    var mouseDownFlag = false;
+    var _this = this;
     var offset = {}
     document.body.addEventListener('mousedown', function (e) {
       var result = _this._isClickSelf(e)
       if (result.is) {
         console.log('click ball')
-        _this.chain = new Chain(_this, _this.context)
-        mouseDownFlag = true
-        offset = result.offset
+        _this.chain = new Chain(_this, _this.context);
+        mouseDownFlag = true;
+        _this.speedX = 0;
+        _this.speedY = 0;
+        offset = result.offset;
       }
     })
     document.body.addEventListener('mousemove', function (e) {
@@ -128,16 +145,17 @@
       }
     })
     document.body.addEventListener('mouseup', function (e) {
+      if (!mouseDownFlag) return;
       mouseDownFlag = false;
       var center = _this._computedCenter();
       var distance = _this._computedDistance(center.x, center.y, _this.chain.x, _this.chain.y);
       if (distance) {
-        var offsetX = _this.chain.x - center.x
-        var offsetY = _this.chain.y - center.y
+        var offsetX = _this.chain.x - center.x;
+        var offsetY = _this.chain.y - center.y;
         _this.speedX = offsetX / 8;
         _this.speedY = offsetY / 8;
         _this.switchRun(true);
-        _this._addImpactObj(_this.chain, false)
+        _this.addImpactObj(_this.chain, false)
       }
     })
   }
@@ -145,8 +163,12 @@
   Ball.prototype.draw = function () {
     this.context.clearRect(0, 0, this.context.width, this.context.height);
     this.context.drawImage(this.image, this.x, this.y, this.radius * 2, this.radius * 2);
-    this.chain && this.chain.draw(this)
-    this.runFlag && this._run()
+    for(var i = 0, len = this.objNum; i < len; i++) {
+      this.objArr[i].draw();
+    }
+    this.chain && this.chain.draw(this);
+    this.runFlag && this._run();
+    
   }
   // 小球运动
   Ball.prototype._run = function () {
@@ -158,14 +180,14 @@
   }
   // 小球运动开关
   Ball.prototype.switchRun = function (bool) {
-    this.runFlag = bool
+    this.runFlag = bool;
   }
   // 小球碰撞检测
   Ball.prototype._impactChecking = function (Obj) {
     var center = this._computedCenter();
-    var obj = Obj.obj
+    var obj = Obj.obj;
     var radius = obj.radius || 0;
-    var distance = this._computedDistance(center.x, center.y, obj.x, obj.y)
+    var distance = this._computedDistance(center.x, center.y, obj.x, obj.y);
     return {
       is: distance < this.radius + radius,
       obj: obj
@@ -173,40 +195,82 @@
   }
   // 批量碰撞检测
   Ball.prototype._impactCheckingList = function () {
-    for(var i = 0, len = this.impactObj.length; i < len; i++) {
-      var obj = this.impactObj[i]
-      var impact = this._impactChecking(obj)
+    for (var i = 0, len = this.impactObjArr.length; i < len; i++) {
+      var obj = this.impactObjArr[i];
+      var impact = this._impactChecking(obj);
       if (impact.is) {
-        console.log('impacting')
+        console.log('impacting');
         if (obj.bounce) {
           // 防止连续触发碰撞
           if (!this._impactFlag) {
-            this.speedX *= impact.obj.x === undefined ? 1 : -1
-            this.speedY *= impact.obj.y === undefined ? 1 : -1
-            var _this = this
+            this.speedX *= impact.obj.x === undefined ? 1 : -1;
+            this.speedY *= impact.obj.y === undefined ? 1 : -1;
+            var _this = this;
             setTimeout(function () {
-              _this._impactFlag = false
-            }, 1000/50)
-            _this._impactFlag = true
+              _this._impactFlag = false;
+            }, 1000 / 50)
+            _this._impactFlag = true;
           }
           return true
         } else {
-          obj.obj.impact()
-          this._removeImpactObj(obj)
+          var i = this._removeImpactObj(obj);
+          obj.obj.impact();
         }
       }
     }
   }
   // 添加碰撞检测的对象
-  Ball.prototype._addImpactObj = function (obj, bounce) {
-    this.impactObj.push({
+  var _uid = 1;
+  Ball.prototype.addImpactObj = function (obj, bounce) {
+    this.impactObjArr.push({
       obj: obj,
-      bounce: bounce
+      bounce: bounce,
+      _uid: _uid++
     })
   }
   // 移除碰撞检测的对象
   Ball.prototype._removeImpactObj = function (obj) {
-    this.impactObj.splice(this.impactObj.indexOf(obj), 1)
+    var index;
+    for (var i = 0, len = this.impactObjArr.length; i < len; i++) {
+      var o = this.impactObjArr[i];
+      o._uid === obj._uid && (index = i);
+    }
+    return this.impactObjArr.splice(index, 1);
+  }
+  // 场景中的球形物体构造函数
+  var BallObj = function (context) {
+    this.context = context;
+    this.image = new Image();
+    this.x = 0;
+    this.y = 0;
+    this.radius = 8;
+    this.color = '#333333';
+    this.type = 1;
+    this.life = true;
+    this._init();
+  }
+  BallObj.prototype._init = function () {
+    this.image.src = './static/image/obstacle_1.png';
+    this.x = Math.random() * (this.context.width - this.radius * 2)
+    this.y = Math.random() * (this.context.height - this.radius * 2)
+    this.type = Math.round(Math.random() * 0.8)
+  }
+  BallObj.prototype._drawImage = function () {
+    this.context.drawImage(this.image, this.x, this.y, this.radius * 2, this.radius * 2);
+  }
+  BallObj.prototype._drawCircle = function() {
+    if (!this.life) return
+    this.context.fillStyle = this.color;
+    this.context.beginPath();
+    this.context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+    this.context.closePath();
+    this.context.fill();
+  }
+  BallObj.prototype.draw = function() {
+    this.type ? this._drawImage() : this._drawCircle();
+  }
+  BallObj.prototype.impact = function (ball) {
+    this.life = false;
   }
   // 主函数
   var main = function () {
@@ -218,7 +282,7 @@
       requestAnimationFrame(step);
     })
   }
-  
+
   window.onload = main
   // 调整窗口大小
   var animationId
